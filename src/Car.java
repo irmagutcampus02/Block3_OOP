c1634894cad3f469b4884b64c08487b5ac802c91
skochauf <stephan.kochauf@lv.campus02.at>
2021-10-16 13:15:25 +0200

    Refactoring of Car

--- src/Car.java
+++ src/Car.java
@@ -2,16 +2,101 @@
 // class => Schlüsselwort für Klasse
 // Car => Klassenname
 public class Car {
+    // 4 access modifier
+    // keinen => package protected
+    // public => öffentlich
+    // private => privat (nur innerhalb der Klasse)
+    // protected => später
+    private String manufacturer;
+    private String colour;
+    private int productionYear;
+    private String model;
+    private int horsePower;
+    private double co2Emission;
+    private int currentSpeed;
 
-    public String manufacturer;
-    public String colour;
-    public int productionYear;
-    public String model;
-    public int horsePower;
-    public double co2Emission;
-    public int currentSpeed;
+    public void initialize(String manufacturer, String colour, int productionYear, String model) {
+        this.manufacturer = manufacturer;
+        this.colour = colour;
+        this.productionYear = productionYear;
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public double getCo2Emission() {
+        return co2Emission;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
 
     //psvm => brauchen wir nicht
+    public void setManufacturer(String manufacturer){
+        // Parameter / lokale Varialben in Methoden, wie Attribute benennen
+        // if (manufacturer == "Volkswagen")
+        if (manufacturer.equals("Volkswagen")
+                || manufacturer.equals("FIAT")) {
+            this.manufacturer = manufacturer;
+        }
+        else {
+            System.out.println("Falcher Hersteller");
+        }
+    }
 
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setCo2Emission(double co2Emission) {
+        this.co2Emission = co2Emission;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public void print() {
+        System.out.println(this.manufacturer + ", "
+                + this.productionYear);
+    }
+
+    public void accelerate() {
+        currentSpeed += 10;
+    }
+
+    public void accelerate(int speed){
+        currentSpeed += speed;
+    }
 }
--- src/CarDemoApp.java
+++ src/CarDemoApp.java
@@ -7,25 +7,27 @@ public class CarDemoApp {
         Car golf5_G_876TU = new Car(); // damit wird ein Objekt von Car erstellt
         Car fiatPanda_G_123RU = new Car();
 
-        golf5_G_876TU.manufacturer = "Volkswagen";
-        golf5_G_876TU.model = "Golf V";
-        golf5_G_876TU.horsePower = 90;
-        golf5_G_876TU.colour = "black";
-        golf5_G_876TU.co2Emission = 300;
+        golf5_G_876TU.setManufacturer("Volkswagen");
+        golf5_G_876TU.setModel("Golf V");
+        golf5_G_876TU.setHorsePower(90);
+        golf5_G_876TU.setColour("black");
+        golf5_G_876TU.setCo2Emission( 300);
 
-        fiatPanda_G_123RU.manufacturer = "FIAT";
-        fiatPanda_G_123RU.model = "Panda";
-        fiatPanda_G_123RU.colour = "red";
-        fiatPanda_G_123RU.horsePower = 75;
-        fiatPanda_G_123RU.co2Emission = 120;
+        fiatPanda_G_123RU.setManufacturer("FIAT");
+        fiatPanda_G_123RU.setModel("Panda");
+        fiatPanda_G_123RU.setColour("red");
+        fiatPanda_G_123RU.setHorsePower(75);
+        fiatPanda_G_123RU.setCo2Emission(120);
 
-        Car golf5_LB_345LX;
+         Car golf5_LB_345LX;
         golf5_LB_345LX = new Car();
-        golf5_LB_345LX.colour = "black";
-        golf5_LB_345LX.model = "Golf 5";
+        golf5_LB_345LX.initialize("Volkswagen",
+                "black", 1980, "Golf I");
 
-        System.out.println("Golf 5 aus Graz: " + golf5_G_876TU.co2Emission);
-        System.out.println("Fiat: " + fiatPanda_G_123RU.model);
 
+        System.out.println("Golf 5 aus Graz: " + golf5_G_876TU.getModel());
+        System.out.println("Fiat: " + fiatPanda_G_123RU.getModel());
+
+        golf5_G_876TU.accelerate(15);
     }
 }
